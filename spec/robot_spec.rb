f30e4403a6960b344609bdeb3d9d18fbdeb0198f
Cara Chew <carachew@gmail.com>
2017-04-26 10:26:44 +0800

    create first tests

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,17 +1,21 @@
 require_relative 'spec_helper'
-require_relative '../lib/robot'
+require_relative '../robot.rb'
 
 describe Robot do
   before(:context) do
     # initialize calculator
-    @robot_one = Robot.new('AB123')
+    @robot1 = Robot.new(AB123)
   end
 
   # Tests go here
 
   describe 'Initialization' do
+    it 'should create new robot name' do
+      expect(@robot1.name.length).not_to eq(0)
+    end
+
     it 'is an instance of Robot class' do
-      expect(@robot_one).to be_instance_of(Robot)
+      expect(@robot1).to be_instance_of(Robot)
     end
   end
 end
