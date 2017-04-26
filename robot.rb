b9b05fbcad436b9cdae0052e4c825f048cd524b7
Darrell Teo <darrelltzj@gmail.com>
2017-04-26 11:15:47 +0800

    passed test1

--- robot.rb
+++ robot.rb
@@ -1,4 +1,11 @@
 class Robot
-
-
+  attr_accessor :name
+  def initialize(name = reset)
+    @name = name
+  end
+  def reset
+    letter = (0...2).map { (65 + rand(26)).chr }.join
+    num = (0...3).map { rand(9) }.join
+    @name = letter + num
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -4,16 +4,14 @@ require_relative '../robot.rb'
 describe Robot do
   before(:context) do
     # initialize calculator
-    @robot1 = Robot.new(AB123)
+    @robot1 = Robot.new
   end
 
   # Tests go here
-
   describe 'Initialization' do
     it 'should create new robot name' do
       expect(@robot1.name.length).not_to eq(0)
     end
-
     it 'is an instance of Robot class' do
       expect(@robot1).to be_instance_of(Robot)
     end
